cfc17d05591a2e2cf37b7d4dadab79043253c0d2
alexandru-beep <ckerarahpikc@gmail.com>
2025-06-01 14:31:48 +0300

    Overview of the Dashboard finished (Admin)
    modified:   app/(dashboard)/[storeId]/(routes)/page.tsx
      1. this will show the overview of the storeId
      2. will calculate and show
        2.1. total revenue of the all orders and the graph for that
        2.2. total count of the sales and stock
    
    new file:   components/overview.tsx
      1. this is where the graph components lives (charts and stuff)
    
    modified:   package-lock.json
    modified:   package.json

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,14 +1,24 @@
 import { CreditCard, DollarSign, Package } from "lucide-react";
 
+import { formatter } from "@/lib/utils";
 import { Separator } from "@radix-ui/react-separator";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
+import { Overview } from "@/components/overview";
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage = async ({ params }: DashboardPageProps) => {
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
 
   return (
     <div className="flex-col">
@@ -25,7 +35,7 @@ const DashboardPage = async ({ params }: DashboardPageProps) => {
             </CardHeader>
             <CardContent>
               <div className="text-2xl font-bold">
-                100$
+                {formatter.format(totalRevenue)}
               </div>
             </CardContent>
           </Card>
@@ -35,7 +45,7 @@ const DashboardPage = async ({ params }: DashboardPageProps) => {
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">+25</div>
+              <div className="text-2xl font-bold">+{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
@@ -44,10 +54,18 @@ const DashboardPage = async ({ params }: DashboardPageProps) => {
               <Package className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">25</div>
+              <div className="text-2xl font-bold">{stockCount}</div>
             </CardContent>
           </Card>
         </div>
+        <Card className="md:col-span-1">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
