7f58477f52f8ba8e51258b5dbb5f519bb3f8e709
alexandru-beep <ckerarahpikc@gmail.com>
2025-06-01 18:58:10 +0300

    fixing the page.tsx billboards to remove vercel fatal error

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,20 +1,16 @@
 import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-export default async function BillboardPage({
+const BillboardPage = async ({
   params,
 }: {
-  params: {
-    storeId: string;
-    billboardId: string;
-  };
-}) {
+  params: { billboardId: string };
+}) => {
   const billboard = await prismadb.billboard.findUnique({
     where: {
       id: params.billboardId,
     },
   });
-
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
@@ -22,4 +18,6 @@ export default async function BillboardPage({
       </div>
     </div>
   );
-}
+};
+
+export default BillboardPage;
